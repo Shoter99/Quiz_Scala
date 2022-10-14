adb2876b76fe1998dfe4478dee8fd4df806c0926
Shoter99 <shoter998@gmail.com>
2022-10-14 10:34:59 +0200

    created few function to ask questions, collect and check if answer was correct

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,44 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn.readLine
 
-def msg = "I was compiled by Scala 3. :)"
+@main def main: Unit = 
+  println("Welcome to my quiz!")
+  println("Let's begin!")
+  
+  for q <- questions do  
+    askQuestion(q)
+    val answer = takeAnswer()
+    println(if checkAnswer(answer, q) then "Correct!" else "Wrong answer!")
+    println()
+
+
+
+
+case class Question(
+  text : String,
+  answers: Seq[String],
+  correct: "a" | "b" | "c" | "d"
+)
+
+def askQuestion(q : Question) : Unit = 
+  println(q.text)
+  println("Answers: ")
+  q.answers.map(
+    println(_)
+  )
+
+def takeAnswer() : String = 
+  print("What's your answer: ")
+  readLine()
+
+def checkAnswer(a: String, q: Question) : Boolean = 
+  if a.length() == 1 then
+    if a == q.correct then
+      return true
+  false
+
+
+val questions = List(
+  Question("1. What does \"print\" do?", List("a) outputs text to screen", "b) takes input from user", "c) sums list of numbers", "d) sends info to printer"), "a"),
+  Question("2. Which variable is used to store true or false values?", List("a) int", "b) String", "c) Boolean", "d) Float"), "c")
+  )
+  
